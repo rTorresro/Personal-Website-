f2091f7fb8a4f8830e25b7ac2ffc99582b7ed18e
Roger Torres <rogertorressa@gmail.com>
2026-03-08 01:31:34 -0500

    Enlarge terminal and video

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -8,6 +8,7 @@ function SidePanel({ onNavigate }) {
           loop
           muted
           playsInline
+          preload="auto"
         />
       </div>
       <TerminalCTA onNavigate={onNavigate} className="terminal-cta-docked" />
